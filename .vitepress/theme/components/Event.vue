291fcea4cf1a1e8c06af7e7759f0e29840f4b181
Georg Tremmel <g@trembl.org>
2024-09-19 11:17:17 +0900

    Event Component

---
+++ .vitepress/theme/components/Event.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <a :href="href">
+      <div>{{ presenter }}</div>
+      <div>{{ title }}</div>
+      {{ href }}
+    </a>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+    title: string,
+    presenter: string,
+    href: string,
+}>();
+</script>
--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,12 +1,15 @@
 // https://vitepress.dev/guide/custom-theme
 import DefaultTheme from 'vitepress/theme';
 import Figure from './components/Figure.vue';
+import Event from './components/Event.vue';
+
 import './custom.css';
 
 export default {
   extends: DefaultTheme,
   enhanceApp({ app, router, siteData }) {
     app.component( 'Figure', Figure );
+    app.component( 'Event', Event );
   }
 }
 
